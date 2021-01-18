889bdee1515d562df1d4e7520c7c71b79384f6b5
Kyosukewu <kyosukewu2020@gmail.com>
2021-01-18 16:15:51 +0800

    修改管理員權限版面建構

---
+++ backend/edit_admin.php
@@ -0,0 +1,27 @@
+<h2 class="ct">修改管理員權限</h2>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc" value="">
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw" value=""></td>
+    </tr>
+    <tr>
+        <td class="tt ct">權限</td>
+        <td class="pp">
+            <div><input type="checkbox" name="" id="">商品分類與管理</div>
+            <div><input type="checkbox" name="" id="">訂單管理</div>
+            <div><input type="checkbox" name="" id="">會員管理</div>
+            <div><input type="checkbox" name="" id="">頁尾版權區管理</div>
+            <div><input type="checkbox" name="" id="">最新消息管理</div>
+        </td>
+    </tr>
+</table>
+<div class="ct">
+    <button>修改</button>
+    <button>重置</button>
+</div>
