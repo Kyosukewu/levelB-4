384eec137ce649e12df44cb87b2d2c163b7f77b2
Kyosukewu <kyosukewu2020@gmail.com>
2021-01-22 14:30:58 +0800

    商品管理頁切版

---
+++ backend/th.php
@@ -0,0 +1,18 @@
+<h2 class="ct">商品分類</h2>
+<form action="api/add_big.php" method="post">
+    <div class="ct">新增大分類
+        <input type="text" name="big" id="">
+        <input type="submit" value="新增">
+    </div>
+</form>
+<form action="api/add_mid.php" method="post">
+    <div class="ct">新增中分類
+        <select name="big" id="">
+        </select>
+        <input type="text" name="mid">
+        <input type="submit" value="新增">
+    </div>
+</form>
+<hr>
+<h2 class="ct">商品管理</h2>
+<div class="ct"><button onclick="lof('?do=add_goods.php')">新增商品</button></div>
